7b38892497e8adef1224cc481b7cabd75be73a1b
ulysses fedrick <ulyfleen@gmail.com>
2023-10-04 20:08:58 +0100

    added choice of which lab to run

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-print("Which lab would you like to run?")
-lab_number = input()
+while True:
+    print("Which lab would you like to run?")
+    lab_number = input()
 
+    # Build the filename based on the user's input
+    filename = f"Lab{lab_number}.py"
 
-filename = f"Lab{lab_number}.py"
-
-try:
-    with open(filename) as f:
-        code = f.read()
-        exec(code)
-except FileNotFoundError:
-    print(f"Lab{lab_number}.py not found.")
-except Exception as e:
-    print(f"An error occurred: {e}")
+    try:
+        with open(filename) as f:
+            code = f.read()
+            exec(code)
+        break  # Exit the loop if the input is valid and the file is executed
+    except FileNotFoundError:
+        print(f"Lab{lab_number}.py not found. Please enter a valid lab number.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
