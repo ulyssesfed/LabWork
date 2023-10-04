14976b83b405ccfa61bb8f3eead509b9728e75cb
ulysses fedrick <ulyfleen@gmail.com>
2023-10-04 19:51:11 +0100

    commit

---
+++ Lab1.py
@@ -0,0 +1,22 @@
+#task 1.1
+print("Hello World!")
+
+#task 1.2
+print("My name is: ","uly", "and I am ","18", "years old.")
+
+#task 1.3
+print("Python")
+print("is")
+print("fun!")
+#---------------------#
+print("Python\nis\nfun!")
+
+#task 1.4
+print("The answer to the ultimate question is 43")
+
+#challenge task 1.5
+print((42*67)/(89-4))
+
+#challenge task 1.6
+import math
+print(math.sqrt(pow(5,4)))
