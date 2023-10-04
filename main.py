137f0bcd724bd3d51f5cf9ef1cdb16cd9ab35f70
ulysses fedrick <ulyfleen@gmail.com>
2023-10-04 20:09:38 +0100

    input validation no longer just ends the program

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ while True:
     print("Which lab would you like to run?")
     lab_number = input()
 
-    # Build the filename based on the user's input
+
     filename = f"Lab{lab_number}.py"
 
     try:
         with open(filename) as f:
             code = f.read()
             exec(code)
-        break  # Exit the loop if the input is valid and the file is executed
+        break
     except FileNotFoundError:
         print(f"Lab{lab_number}.py not found. Please enter a valid lab number.")
     except Exception as e:
